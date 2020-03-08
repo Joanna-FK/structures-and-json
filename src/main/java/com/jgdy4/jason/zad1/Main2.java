10123b8b91779ad139c81cddbbb5e98a0f5243f1
Joanna Fatyga-Kaminska <jfatyga@wp.pl>
2020-03-08 12:43:56 +0100

    commit initial json zad1

---
+++ src/main/java/com/jgdy4/jason/zad1/Main2.java
@@ -0,0 +1,18 @@
+package com.jgdy4.jason.zad1;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main2 {
+    public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            RecipesResponse response = objectMapper.readValue(new File("recipies.json"), RecipesResponse.class);
+            response.getResults().forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/jgdy4/jason/zad1/RecipeResult.java
@@ -0,0 +1,20 @@
+package com.jgdy4.jason.zad1;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString (of= {"id", "title"})
+public class RecipeResult {
+    private int id;
+    private String title;
+    private int readyInMinutes;
+    private int servings;
+    private String image;
+    private List<String> imageUrls;
+}
