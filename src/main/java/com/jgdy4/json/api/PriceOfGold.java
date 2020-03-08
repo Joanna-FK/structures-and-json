5f05dc5310a9f5cf53b00d5c2793b9be1d4c7e32
Joanna Fatyga-Kaminska <jfatyga@wp.pl>
2020-03-08 15:25:14 +0100

    commit json and structures

---
+++ src/main/java/com/jgdy4/json/api/PriceOfGold.java
@@ -0,0 +1,15 @@
+package com.jgdy4.json.api;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class PriceOfGold {
+    private String data;
+    private Double cena;
+}
